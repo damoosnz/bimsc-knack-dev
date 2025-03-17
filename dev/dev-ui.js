191ff926cadd5152bd1e1b6804c43cb165f81738
damoosnz <damien.legrand@bimsolutionscentre.com>
2025-03-17 21:18:16 +1300

    added dev bar

--- dev/dev-ui.js
+++ dev/dev-ui.js
@@ -1,25 +1,64 @@
 const $knackNavBar = $('.kn-info-bar')
 
-const $devUi = `
-    <div class="fas-dev">
-        <div class="fas-ui>
-            <label>Dev Mode:</label>
-            <input type="radio" name="devMode" value="yes"> Yes
-            <input type="radio" name="devMode" value="no" checked> No
+// Retrieve values from localStorage (moved outside the function)
+const knackLs = {
+    devMode: localStorage.getItem("knackDevMode") || "no",
+    showDevMode: localStorage.getItem("showDevEnvInfo") || "no",
+    consDevMode: localStorage.getItem("showDevConsInfo") || "no"
+};
 
-            <label>Show Dev Mode:</label>
-            <input type="radio" name="showDevMode" value="yes"> Yes
-            <input type="radio" name="showDevMode" value="no" checked> No
+addDevBar()
 
-            <label>Log Dev Mode:</label>
-            <input type="radio" name="logDevMode" value="yes"> Yes
-            <input type="radio" name="logDevMode" value="no" checked> No
 
-            <button id="reloadBtn">Reload</button>
+function addDevBar() {
+    // Prepare the HTML for the dev bar
+    const $devUi = `
+        <div class="fas-dev">
+            <div class="fas-ui">
+                <label>Dev Mode:</label>
+                <input type="radio" name="devMode" value="yes"> Yes
+                <input type="radio" name="devMode" value="no"> No
 
+                <label>Show Dev Mode:</label>
+                <input type="radio" name="showDevMode" value="yes"> Yes
+                <input type="radio" name="showDevMode" value="no"> No
+
+                <label>Log Dev Mode:</label>
+                <input type="radio" name="logDevMode" value="yes"> Yes
+                <input type="radio" name="logDevMode" value="no"> No
+
+                <button id="reloadBtn">Reload</button>
+            </div>
         </div>
-    </div>
-                `
+    `;
+
+    // Set radio button checked state based on knackLs values
+    $(`input[name="devMode"][value="${knackLs.devMode}"]`).prop("checked", true);
+    $(`input[name="showDevMode"][value="${knackLs.showDevMode}"]`).prop("checked", true);
+    $(`input[name="logDevMode"][value="${knackLs.consDevMode}"]`).prop("checked", true);
+
+    // Reload button click event
+    $("#reloadBtn").on("click", function () {
+        // Update localStorage with selected values
+        localStorage.setItem("knackDevMode", $(`input[name="devMode"]:checked`).val());
+        localStorage.setItem("showDevEnvInfo", $(`input[name="showDevMode"]:checked`).val());
+        localStorage.setItem("showDevConsInfo", $(`input[name="logDevMode"]:checked`).val());
+
+        // Reload the page
+        location.reload();
+    });
+
+    // Insert the UI into the DOM after the navbar
+    $($devUi).insertAfter($knackNavBar); // Append UI to DOM
+}
 
 
-$($devUi).insertAfter($knackNavBar);
+
+
+
+
+
+
+
+
+
