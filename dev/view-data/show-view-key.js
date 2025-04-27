e5441c903ad47a933c0a41eaa692e0a5a5707c7c
damoosnz <damien.legrand@bimsolutionscentre.com>
2025-04-27 11:54:49 +0200

    added view type

--- dev/view-data/show-view-key.js
+++ dev/view-data/show-view-key.js
@@ -1,5 +1,5 @@
 export function get$ViewKey(view) {
 
-    return $(`<div class="bimsc-knack-dev"><b>${view.scene.key} ${view.key} (${view.scene.slug})</b></div>`)
+    return $(`<div class="bimsc-knack-dev"><b>${view.scene.key} ${view.key} (${view.scene.slug}) view type: ${view.type}</b></div>`)
 
 }
