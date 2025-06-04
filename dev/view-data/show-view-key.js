3ff93f80c277370a17c1a9f0058a0172645a116c
damoosnz <damien.legrand@bimsolutionscentre.com>
2025-06-04 16:43:20 +1200

    updated get$ViewKey to accomodate calendar event detail view

--- dev/view-data/show-view-key.js
+++ dev/view-data/show-view-key.js
@@ -1,5 +1,10 @@
 export function get$ViewKey(view) {
 
-    return $(`<div class="bimsc-knack-dev"><b>${view.scene.key} ${view.key} (${view.scene.slug}) view type: ${view.type}</b></div>`)
+    try {
+        return $(`<div class="bimsc-knack-dev"><b>${view.scene.key} ${view.key} (${view.scene.slug}) view type: ${view.type}</b></div>`)
+    } catch (err) {
+        return $(`<div class="bimsc-knack-dev"><b>${view.key}</b></div>`)
+    }
+
 
 }
